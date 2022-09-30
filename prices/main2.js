850a946661d76ae42fdc066ff745b73e9564ce85
Kewbin <47226508+ccrecipes@users.noreply.github.com>
2022-09-30 12:43:25 +0200

    Update main2.js

--- prices/main2.js
+++ prices/main2.js
@@ -4,7 +4,15 @@ $.ajax({
     'dataType': "json",
     success: function(data) {
 
-    console.log(data);
+    for (item in data) {
+        name = data[item]["item"]["name"]+"<br>"+data[item]["item"]["price"]["min"]+" - "+data[item]["item"]["price"]["max"]
+        img = "https://www.cubiccastles.com/recipe_html/"+data[item]["item"]["url"]+".png";
+        txt = `<div class='item'><img src=${img}></div>`;
+        //txt += `<div class='item'><img src=${img}><p>${name}</p></div>`;
+        //{console.log(json[item]["type"]["name"])};
+        //{console.log(json[item]["item"]["name"]+": "+json[item]["item"]["price"]["min"]+" - "+json[item]["item"]["price"]["max"])};
+        document.getElementById('prices').innerHTML += txt;
+    }
 
     },
     error: function(){
