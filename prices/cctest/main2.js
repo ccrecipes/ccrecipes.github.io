762e5e5db3ffae489fabc636f4e48a6847ec4711
Kewbin <47226508+ccrecipes@users.noreply.github.com>
2022-10-03 11:53:15 +0200

    update

--- prices/cctest/main2.js
+++ prices/cctest/main2.js
@@ -9,11 +9,11 @@ $.ajax({
         //img = "https://www.cubiccastles.com/recipe_html/"+data[item]["item"]["url"]+".png";
         img = data[item]["item"]["url"]+" sprite";
         //txt += `<div class='item'><img src=${img}></div>`;
-        txt += `<div class='item'><span class=${img}></span></div>`;
+        txt = `<div class='item'><span class=${img}></span></div>`;
         //txt += `<div class='item'><img src=${img}><p>${name}</p></div>`;
         //{console.log(json[item]["type"]["name"])};
         //{console.log(json[item]["item"]["name"]+": "+json[item]["item"]["price"]["min"]+" - "+json[item]["item"]["price"]["max"])};
-        document.getElementById('prices').innerHTML = txt;
+        document.getElementById('prices').innerHTML += txt;
         console.log(txt)
     }
 
