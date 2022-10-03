2722227e9ba7f3cffcedd579388b9dd050891419
Kewbin <47226508+ccrecipes@users.noreply.github.com>
2022-10-03 14:52:20 +0200

    Update main2.js

--- prices/cctest/main2.js
+++ prices/cctest/main2.js
@@ -1,15 +1,16 @@
 $.ajax({
     'type': "Get",
+    'async': false,
+    'global': true,
     'url': "prices.json",
     'dataType': "json",
     success: function(data) {
 
     for (item in data) {
         name = data[item]["item"]["name"]+"<br>"+data[item]["item"]["price"]["min"]+" - "+data[item]["item"]["price"]["max"]
-        //img = "https://www.cubiccastles.com/recipe_html/"+data[item]["item"]["url"]+".png";
         img = data[item]["item"]["url"];
         //txt += `<div class='item'><img src=${img}></div>`;
-        txt = `<div class='item'><span class='${img} sprite'></span></div>`;
+        txt = `<div class='item'><span class='${img} sprite'></span><p>${name}</p></div>`;
         //txt += `<div class='item'><img src=${img}><p>${name}</p></div>`;
         document.getElementById('prices').innerHTML += txt;
     }
