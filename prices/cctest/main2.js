464add75553c5616fa04805cd431d6b66fb49e62
Kewbin <47226508+ccrecipes@users.noreply.github.com>
2022-10-03 12:05:46 +0200

    fix

--- prices/cctest/main2.js
+++ prices/cctest/main2.js
@@ -7,9 +7,9 @@ $.ajax({
     for (item in data) {
         name = data[item]["item"]["name"]+"<br>"+data[item]["item"]["price"]["min"]+" - "+data[item]["item"]["price"]["max"]
         //img = "https://www.cubiccastles.com/recipe_html/"+data[item]["item"]["url"]+".png";
-        img = data[item]["item"]["url"]+" sprite";
+        img = data[item]["item"]["url"];
         //txt += `<div class='item'><img src=${img}></div>`;
-        txt = `<div class='item'><span class=${img}></span></div>`;
+        txt = `<div class='item'><span class='${img} sprite'></span></div>`;
         //txt += `<div class='item'><img src=${img}><p>${name}</p></div>`;
         //{console.log(json[item]["type"]["name"])};
         //{console.log(json[item]["item"]["name"]+": "+json[item]["item"]["price"]["min"]+" - "+json[item]["item"]["price"]["max"])};
