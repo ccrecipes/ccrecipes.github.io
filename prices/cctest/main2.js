7102c56572232b95aee4c2cf19ceacb40e2a843e
Kewbin <47226508+ccrecipes@users.noreply.github.com>
2022-10-04 14:04:17 +0200

    fix

--- prices/cctest/main2.js
+++ prices/cctest/main2.js
@@ -7,10 +7,10 @@ $.ajax({
     success: function(data) {
 
     for (item in data) {
-        name = data[item]["item"]["name"]+"<br>"+"<p style='font-size:67%;'>"+data[item]["item"]["price"]["min"].toLocaleString()+"<b> - </b>"+data[item]["item"]["price"]["max"].toLocaleString()+"</p>";
+        name = "<p>"+data[item]["item"]["name"]+"<br>"+"<p style='font-size:67%;'>"+data[item]["item"]["price"]["min"].toLocaleString()+"<b> - </b>"+data[item]["item"]["price"]["max"].toLocaleString()+"</p>"+"</p>";
         img = data[item]["item"]["url"];
         //txt += `<div class='item'><img src=${img}></div>`;
-        txt = `<div class='item'><span class='${img} sprite'></span><p>${name}</p></div>`;
+        txt = `<div class='item'><span class='${img} sprite'></span>${name}</div>`;
         //txt += `<div class='item'><img src=${img}><p>${name}</p></div>`;
         document.getElementById('prices').innerHTML += txt;
     }
