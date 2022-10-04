058ca696c26161b28c75512255953a6eeaa6d380
Kewbin <47226508+ccrecipes@users.noreply.github.com>
2022-10-04 13:22:44 +0200

    Update main2.js

--- prices/cctest/main2.js
+++ prices/cctest/main2.js
@@ -7,7 +7,7 @@ $.ajax({
     success: function(data) {
 
     for (item in data) {
-        name = data[item]["item"]["name"]+"<br>"+data[item]["item"]["price"]["min"]+" - "+data[item]["item"]["price"]["max"]
+        name = data[item]["item"]["name"]+"<br>"+data[item]["item"]["price"]["min"].toLocaleString()+"<b>-</b>"+data[item]["item"]["price"]["max"].toLocaleString()
         img = data[item]["item"]["url"];
         //txt += `<div class='item'><img src=${img}></div>`;
         txt = `<div class='item'><span class='${img} sprite'></span><p>${name}</p></div>`;
