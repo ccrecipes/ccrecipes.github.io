ca64784332fe1a2f39fb1dc15e103b3b31d3dda4
Kewbin <47226508+ccrecipes@users.noreply.github.com>
2022-10-10 18:10:32 +0200

    fixed bug

--- prices/cctest/main2.js
+++ prices/cctest/main2.js
@@ -10,13 +10,11 @@ $.ajax({
         color = {"Stable":"5px solid #ffffff","Increasing":"5px solid #1fc44b","Decreasing":"5px solid #c41a1a"};
         name = data[item]["item"]["name"]+"<br>"+data[item]["item"]["price"]["min"].toLocaleString()+"<b> - </b>"+data[item]["item"]["price"]["max"].toLocaleString()
         img = data[item]["item"]["url"];
-        //txt = `<div style="border-bottom: ${color[data[item]["item"]["state"]]}" class='item'><span class='${img} sprite'></span><p>${name}</p></div>`;0
 
         if (data[item]["item"]["url"] == "") {
             txt = `<div style="border-bottom: ${color[data[item]["item"]["state"]]}" class='item'><span><img src="../cctest/error.png"></span><p>${name}</p></div>`;
         }
 
-        //document.getElementById('prices').innerHTML += txt;
         $('#prices').append(`<div style="border-bottom: ${color[data[item]["item"]["state"]]}" class='item'><span class='${img} sprite'></span><p>${name}</p></div>`)
     };
 
