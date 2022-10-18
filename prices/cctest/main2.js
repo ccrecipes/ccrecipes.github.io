9c55e0f95ac2adc95333cf2bc80c946171a47415
Kewbin <47226508+ccrecipes@users.noreply.github.com>
2022-10-18 12:22:56 +0200

    update

--- prices/cctest/main2.js
+++ prices/cctest/main2.js
@@ -15,8 +15,30 @@ $.ajax({
             txt = `<div style="border-bottom: ${color[data[item]["item"]["state"]]}" class='item'><span><img src="../cctest/error.png"></span><p>${name}</p></div>`;
         }
 
-        $('#prices').append(`<div style="border-bottom: ${color[data[item]["item"]["state"]]}" class='item'><span class='${img} sprite'></span><p>${name}</p></div>`)
+        //$('#prices').append(`<div style="border-bottom: ${color[data[item]["item"]["state"]]}" class='item'><span class='${img} sprite'></span><p>${name}</p></div>`)
+
+        wearables = ["Hat Pack","Wing Pack","Critter Suit Pack","Accessories Pack","Clothes Pack","Clothes Item","Quest","Virus","Dungeon Pack","Track Pack","Kitchen Pack","Ocean Pack","Sci-fi Pack","Steampunk Pack","Adventure Pack","Fishing Pack","April Fools' Day","Easter","Valentine's Day'","Summer","Fan","Halloween","Thanksgiving","Christmas","Farm Pack","Wands","New Year's Day"]
+
+        for (wearable in wearables) {
+            if (data[item]["type"]["section"] == wearables[wearable].toUpperCase()) {
+                $("#wearables_prices").append(`<div style="border-bottom: ${color[data[item]["item"]["state"]]}" class='item'><span class='${img} sprite'></span><p>${name}</p></div>`)
+            };
+        }
+        if (data[item]["type"]["section"] == "CARS") {
+            $("#cars_pets").append(`<div style="border-bottom: ${color[data[item]["item"]["state"]]}" class='item'><span class='${img} sprite'></span><p>${name}</p></div>`)
+        };
     };
+
+    $(".searchbar span").on("click", function(event) {
+        $(".sections").css({"display":"block"})
+    });
+
+    $(document).mouseup(function(event) {
+        var sections = $(".sections");
+        if (!sections.is(event.target) && sections.has(event.target).length === 0) {
+            sections.hide();
+        }
+    });
 
     $(document).ready(function() {
         $("#filterInput").on("keyup", function() {
