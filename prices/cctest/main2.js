9c078d111a47de3fb461cee3a5631190c647f46d
Kewbin <47226508+ccrecipes@users.noreply.github.com>
2022-11-08 22:45:29 +0100

    Update main2.js

--- prices/cctest/main2.js
+++ prices/cctest/main2.js
@@ -80,6 +80,7 @@ for (item in basic) {
 $(".sections h1").click(function() {
     //window.location.href += encodeURI($(this).text().toLowerCase());
     //window.location.href = window.location.origin + "/prices/cctest/section?section=" + encodeURI($(this).text().toLowerCase());
+    window.location.pathname += "/";$(this).text().toLowerCase();
     tittle_section = $(this).text();
     id_section = $(this).text().toLowerCase().replace(" ","_").replace("'s ","").replace("' ","_");
     $(".all_prices").append(`<div id=${id_section} class="list"><h1 class="header">${tittle_section}</h1></div>`);
